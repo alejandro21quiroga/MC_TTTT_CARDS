98223c49d0ee1cde8e8465d1ad6524c8b0da596b
Alejandro Quiroga Trivino <alejandro.quiroga@correounivalle.edu.co>
2025-05-12 11:58:25 +0200

    tt

--- master.sh
+++ master.sh
@@ -2,5 +2,5 @@
 
 git add .
 git commit -m "tt"
-git remote add origin git@github.com:alejandro21quiroga/MC_TTTT_SIMULATION.git
+#git remote add origin git@github.com:alejandro21quiroga/MC_TTTT_SIMULATION.git
 git push -u origin main
